104ca5c69a709b8e1adc4d203aff35fe84420a00
aslanator <aslanator@mail.ru>
2019-04-16 21:34:04 +0300

    add basic method to bookmark class

--- lib/bookmark.php
+++ lib/bookmark.php
@@ -6,6 +6,14 @@ use Brainkit\Data\DataTable;
  
 class Bookmark{
 
-  
+    public static function get($cond) {
+        $result = DataTable::getList(
+                        array(
+                            'select' => array('*')
+        ));
+        $row = $result->fetch();
+        print "<pre>"; print_r($row); print "</pre>";
+        return $row;
+    }
  
 }
