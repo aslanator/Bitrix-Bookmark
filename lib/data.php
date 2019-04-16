79afa39e4ad47fa09fb21d558cb46ae5827bc9d9
aslanator <aslanator@mail.ru>
2019-04-16 21:28:02 +0300

    create data

---
+++ lib/data.php
@@ -0,0 +1,69 @@
+<?php
+namespace Bookmark\D7;
+ 
+use Bitrix\Main\Entity;
+use Bitrix\Main\Localization\Loc;
+  
+
+/**
+ * class BookmarkTable
+ *  */ 
+
+class DataTable extends Entity\DataManager
+{
+   /**
+    * Returns DB table name for entity.
+    *
+    * @return string
+    */
+   public static function getTableName()
+   {
+      return 'bookmark';
+   }
+ 
+   /**
+    * Returns entity map definition.
+    *
+    * @return array
+    */
+   public static function getMap()
+   {
+      return array(
+         'ID' => array(
+            'data_type' => 'integer',
+            'primary' => true,
+            'autocomplete' => true,
+            'title' => Loc::getMessage('DATA_ENTITY_ID_FIELD'),
+         ),
+         'FAVICON' => array(
+            'data_type' => 'text',
+            'title' => Loc::getMessage('DATA_ENTITY_FAVICON_FIELD'),
+         ),
+         'URL' => array(
+            'data_type' => 'text',
+            'required' => true,
+            'title' => Loc::getMessage('DATA_ENTITY_URL_FIELD'),
+         ),
+        'TITLE' => array(
+            'data_type' => 'string',
+            'required' => true,
+            'title' => Loc::getMessage('DATA_ENTITY_TITLE_FIELD'),
+         ),
+        'META_DESCRIPTION' => array(
+            'data_type' => 'string',
+            'required' => true,
+            'title' => Loc::getMessage('DATA_ENTITY_META_DESCRIPTION_FIELD'),
+         ),
+        'META_KEYWORDS' => array(
+            'data_type' => 'string',
+            'required' => true,
+            'title' => Loc::getMessage('DATA_ENTITY_META_KEYWORDS_FIELD'),
+         ),
+         'CREATED' => array(
+            'data_type' => 'datetime',
+            'required' => true,
+            'title' => Loc::getMessage('DATA_ENTITY_CREATED_FIELD'),
+         ),
+      );
+   }
+}
